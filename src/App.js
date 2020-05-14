e7da274784029ecf9256ecf1dd44dfde43f71600
jona35q <jona.atom53@gmail.com>
2020-05-14 09:38:03 -0500

    Agregando la carpeta components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Header from './components/Header'
 
 function App() {
   return (
     <>
+      <Header />
       <h1>Hola soy Jonathan</h1>
       <p>Aqui ira la descripcion del proyecto, tomar en cuenta la situacion.</p>
     </>
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <h1>Este es el encabezado</h1>
+        </div>
+    )
+}
+
+export default Header
