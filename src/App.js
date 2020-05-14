cbdb48c749003c98a4c72cc36736a90d28ea5485
Jonathan German <yarvis.jona@gmail.com>
2020-05-14 09:00:08 -0500

    Se agrego un parrafo en App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React from 'react';
 
 function App() {
   return (
-    <h1>Hola soy Jonathan</h1>
+    <>
+      <h1>Hola soy Jonathan</h1>
+      <p>Aqui ira la descripcion del proyecto, tomar en cuenta la situacion.</p>
+    </>
   );
 }
 
